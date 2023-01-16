0b820da2a2ff3c3e3773da736b3516c61855e720
Colten Siemergit <colten50@hotmail.com>
2023-01-16 11:56:25 -0500

    worked on js

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,27 @@
 // BUTTONS 
 const backThisProjectBtn = document.querySelector(".backtoproj"); 
-
+const selectRadio1 = document.querySelector("selected-1"); 
+const selectRadio2 = document.querySelector("selected-2"); 
+const selectRadio3 = document.querySelector("selected-3"); 
 
 // DISPLAYS 
 const modelContainer = document.getElementById("model-container"); 
-
-
-
+const plegeOne = document.querySelector("pledge-1"); 
+const plegeTwp = document.querySelector("pledge-2"); 
+const plegeThree = document.querySelector("pledge-3"); 
 
 
 //Open modal selection 
 backThisProjectBtn.addEventListener('click', (e) => { 
     e.preventDefault(); 
-
-    console.log("hello")
-// if(modelContainer.style.display === 'none'){
-//     modelContainer.style.display= block;
-// console.log('hello')
-//  }
+    modelContainer.style.display= "block";
+}); 
 
 
+// Select RADIO 
+
+selectRadio1.addEventListener('click', (e) => { 
+    e.preventDefault(); 
+    console.log("works")
+    plegeOne.style.display="block"; 
 })
