51faf66aed757540d3a41226c1aa507f43a4961d
Colten Siemergit <colten50@hotmail.com>
2023-01-21 10:43:14 -0500

    updated

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,19 @@ const disable = true;
 // BUTTONS
 const backThisProjectBtn = document.querySelector(".backtoproj");
 const selectPledge = document.querySelectorAll('input[name="radio"]');
-const selectReward = document.querySelectorAll(".reward-btn");
+const selectReward = document.querySelectorAll(".reward-btn"); 
+const continuePledge = document.querySelectorAll('.pledge-reward-btn'); // continue button in select reward
 const selectRadio = document.querySelectorAll(".selected-radio"); // radio buttons 
 const closeBTN = document.querySelector(".close-btn");
+const inputPledgeValue= document.getElementsByName("costpledge"); // placeholder values for input 
 
 // DISPLAYS
 const modelContainer = document.getElementById("model-container"); // Model Container
 const pledgeContainer = document.querySelectorAll(".pledge"); // Pledge containers 
 
 //HTML DISPLAY CLASSES
-const valueBackedhtml = document.querySelector("amount-backed");
+const valueBackedhtml = document.querySelector(".amount-backed");
+const totalBackershtml = document.querySelector(".total-backed"); 
 const title = document.querySelectorAll(".item-title"); //titles
 const pledge = document.querySelectorAll(".item-pledge"); // pledges min amounts
 const itemPara = document.querySelectorAll(".item-para"); // Paragraphs
@@ -22,16 +25,19 @@ const productPledge = document.querySelectorAll(".product-pledge");
 const productPara = document.querySelectorAll(".pledge-para");
 const productLeft = document.querySelectorAll(".pledge-left");
 
+
+
 //Starting values
-const valueBacked = 89914;
-const totalBackers = 5007;
-const daysleft = 56;
+let valueBacked = 89914;
+let totalBackers = 5007;
+let daysleft = 56;
 
 // Item Products
 const products = [
   {
     id: 1,
     title: "Bambo Stand",
+    min: 25,
     pledge: "Pledge $25 or more.",
     paragraph:
       "You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and you’ll be added to a special Backer member list.",
@@ -40,6 +46,7 @@ const products = [
   {
     id: 2,
     title: "Black Edition Stand",
+    min: 75,
     pledge: "Pledge $75 or more.",
     paragraph:
       "You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
@@ -49,6 +56,7 @@ const products = [
   {
     id: 3,
     title: "Mahongany Special Edition",
+    min: 200,
     pledge: "Pledge $200 or more.",
     paragraph:
       "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added to our Backer member list. Shipping is included.",
@@ -68,6 +76,10 @@ products.forEach((obj, index) => {
   productLeft[index].textContent = obj.left;
 });
 
+// Display of back and backers
+valueBackedhtml.textContent= valueBacked; 
+totalBackershtml.textContent=totalBackers; 
+
 // IF Items left are zero
 function stockInventory() {
   itemLeft.forEach(e => {
@@ -82,11 +94,11 @@ function stockInventory() {
  productLeft.forEach(e => { 
   if (parseInt(e.textContent) === 0) { 
     e.closest(".item-containers").style.opacity = 0.4;
-    e.parentElement.nextElementSibling.textContent="Out of Stock" 
-    e.parentElement.nextElementSibling.disabled=true;  
-  }
+    e.parentElement.nextElementSibling.textContent="Out of Stock";
+    // e.parentElement.parentElement.firstChild.style.color="red"; 
+
 }
-)
+})
 }
 stockInventory();
 
@@ -112,16 +124,48 @@ closeBTN.addEventListener("click", (e) => {
 //Open Radios
 selectRadio.forEach(e => { 
   e.addEventListener('click',() => { 
+      document.querySelectorAll('.item-containers').forEach(e => { 
+        e.classList.remove('borderActive'); 
+      })
+
+      pledgeContainer.forEach(e => { 
+        e.classList.add("visible")
+      })
+
+
     if(e.checked) { 
       e.parentElement.parentElement.lastElementChild.classList.remove("visible"); 
       e.parentElement.parentNode.classList.add('borderActive');
     }
 
-    else { 
-      e.parentElement.parentNode.classList.remove('borderActive');
-    }
+    
       
     }) 
 
 })
   
+
+//MODEL INPUT PlaceHOLDER 
+products.forEach((obj, index) => { 
+  inputPledgeValue[index].placeholder = obj.min; 
+  inputPledgeValue[index].setAttribute('min', obj.min); 
+})
+
+
+
+
+// add input to AMOUNT BACKed 
+
+continuePledge.forEach(e => { 
+  e.addEventListener( "click", () => { 
+    const inputValue = e.closest("cost-pledge"); 
+    const inputMin = inputValue.value; 
+    
+    console.log(inputMin); 
+
+
+  }
+    //take value in input field 
+    //add to total backers 
+)}
+)
